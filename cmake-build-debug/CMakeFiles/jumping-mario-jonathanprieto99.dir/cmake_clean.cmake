150bcd7a6bbefecb6e7f5438f3da92dd4cfe32ff
Jonathan Prieto <jonathan.prieto@me.com>
2019-08-25 20:06:54 -0500

    ok

---
+++ cmake-build-debug/CMakeFiles/jumping-mario-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jumping-mario-jonathanprieto99.dir/main.cpp.o"
+  "jumping-mario-jonathanprieto99.pdb"
+  "jumping-mario-jonathanprieto99"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jumping-mario-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/jumping-mario-jonathanprieto99.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for jumping-mario-jonathanprieto99.
+# This may be replaced when dependencies are built.
