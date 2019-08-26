b1be8fbed706817ebd3934bbca36e83c2ef0d07f
Jonathan Prieto <jonathan.prieto@me.com>
2019-08-25 20:06:39 -0500

    ok

--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
-#include <iostream>
-
+#include<iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include<algorithm>
 using namespace std;
 
-int main() {
-	return EXIT_SUCCESS;
-}
+int main(){
+	int T,N,H,L;
+	cin >> T;
+	vector<int> v(50);
+	for(int i=0; i<T;i++){
+		H =0;
+		L =0;
+		cin >> N;
+		if(T == 1){
+			cout << "Case " << T << ": " << H <<  " " << L << endl;
+			continue;
+		}
+		int j=0;
+		while(j<N)
+		{
+			cin >> v[j];
+			j++;
+		}
+		int k=0;
+		while(k<N-1){
+			if(v[k] < v[k+1]){
+				H++;
+			}
+			else if (v[k] > v[k+1]){
+				L++;
+			}
+			k++;
+		}
+		cout << "Case " << i+1 << ": " << H <<  " " << L << endl;
 
+	}
+	return 0;
+}
